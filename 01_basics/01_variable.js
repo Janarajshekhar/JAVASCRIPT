c0e69b82a06569111c3e75575efc09663fb116ce
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-05-25 10:53:01 +0530

    Initial commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountid = 123456
+let accountEmail = "hello@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+let accountState
+// accountid = 2 //not allowed
+
+accountEmail="world@gmail.com"
+accountPassword = "94856"
+accountCity= "puna"
+
+console.log(accountid);
+console.table([accountid, accountEmail, accountPassword, accountCity, accountState]);
