45b4f67a7cfe19a66b9ec31aff4cddbfe7c8d091
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-05-27 22:56:24 +0530

    Add initial implementation of data types and stack vs heap concepts

---
+++ 01_basics/07_stack_heap.js
@@ -0,0 +1,19 @@
+let myYoutubename = "hiteshchoudharydotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userone ={
+    email: "njhflsfj@gmail.com",
+    upi: "user@ybl"
+}
+let usertwo = userone
+
+usertwo.email = "jdskjkasjdlja@gmail.com"
+usertwo.upi = "dndnn@ybl"
+
+console.log(userone.email);
+console.log(userone.upi);
+console.log(usertwo.email);
+console.log(usertwo.upi);
